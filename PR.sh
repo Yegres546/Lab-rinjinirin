3e83efd1ba3f4d8c2c95fa72638f3f3f90981001
Yegres546 <126583849+Yegres546@users.noreply.github.com>
2025-11-24 00:20:09 +0200

    Add files via upload
    PR

---
+++ PR.sh
@@ -0,0 +1,5 @@
+git checkout -b lab1-sonar-setup
+git add .github/workflows/sonarcloud.yml
+git commit -m "feat: add SonarCloud analysis workflow"
+git push origin lab1-sonar-setup
+# Створіть PR на GitHub
